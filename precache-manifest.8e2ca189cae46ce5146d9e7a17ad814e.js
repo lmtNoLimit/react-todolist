cd7249bc2747e82827050386e0b2981cf1495d49
Lâm Minh Thành <lmt151099@gmail.com>
2019-04-27 21:30:00 +0700

    Updates

---
+++ precache-manifest.8e2ca189cae46ce5146d9e7a17ad814e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfb997c28bd7864ec3112014ae73bfc",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "bd18b020c200aef1d155",
+    "url": "/react-todolist/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bd18b020c200aef1d155",
+    "url": "/react-todolist/static/js/2.8f791c4b.chunk.js"
+  },
+  {
+    "revision": "184a58337f685c398bb9",
+    "url": "/react-todolist/static/js/main.3c6ef3f5.chunk.js"
+  },
+  {
+    "revision": "dd2a0c730e02a59d4852",
+    "url": "/react-todolist/static/js/runtime~main.8b2e420c.js"
+  }
+]);
